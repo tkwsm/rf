c3cad1d43610619a1f809b195e4528d36fdfb95a
Tkwsm <kawashima38@gmail.com>
2021-10-03 09:12:19 +0900

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "RF Genome Browser"
   end
 
   test "should get help" do
