b2545ce4e79ede3705b7165bc2ac0a5518209827
tkwsm <kawashima38@gmail.com>
2022-12-15 18:51:43 +0900

    add some test/models/*rb files

---
+++ test/models/protein_seq_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ProteinSeqTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/tattribute_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class TattributeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
